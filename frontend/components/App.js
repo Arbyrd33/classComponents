c9facf92eb01c2bfeba896b4cc47efb3bd38814d
Arbor Matos <arbor@MacBook-Pro.local>
2022-10-08 11:25:16 -0700

    State renders in App.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import Form from './Form';
 
 
+
 export default class App extends React.Component {
   constructor(){
     super();
-    this.state = {};
+    this.state = {
+        name: 'Organize Garage',
+        id: 1528817077286, // could look different, you could use a timestamp to generate it
+        completed: false
+      };
   }
+
   render() {
     return (
       <>
         <div>
-          This is my to to app! I hated what I'd built so far so instead of just staring at it I totally nuked it! I will understand the changes made to this course!
+          This is my to to app! I hated what I'd built so far so instead of just staring at it I totally nuked it! I will understand the changes made to this course! I need to {this.state.name} today.
         </div>
         <Form/>
       </>
     )
   }
 }
-// - OK! `<App />` will hold all the data needed for this project.
-//   - All of your application data will be stored here on `<App />`.
-//   - All of your `handler` functions should live here on `<App />`.
